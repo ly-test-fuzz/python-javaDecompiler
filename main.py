86ebf4ceffe8fbd63b8c0d98b90f363ce88a1cb4
java <3280907645@qq.com>
2022-09-06 08:10:50 -0700

    初始化一个项目 - 传参 : 源路径 目的路径 - 解包所有的 war 包 , 作为 .war.folder - 会将原路径所有文件拷贝到目的路径，然后反编译其中的所有 .class ， .jar - 所有的 .jar 会被反编译为 .jar .folder 文件 - 然后删除原路径中所有的 .jar 收集到目的路径下的 storlib 目录中 ， - 清理准备过程中的 .war.folder

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import shutil
+from sys import argv
+
+def main():
+    if len(argv) < 3:
+        print("Usage: \n\t[decompiler] sourcePath dstPath [jarPath]")
+        exit(0)
+    sourcePath, dstPath = argv[1], argv[2]
+    if len(argv) == 4:
+        jarPath = argv[3]
+    else:
+        jarPath = "/home/java/Desktop/tool/Jetbrains/idea-IU-222.3739.54/plugins/java-decompiler/lib/java-decompiler.jar"
+
+
+    if os.path.exists(dstPath) :
+        shutil.rmtree(dstPath)
+    os.mkdir(dstPath)
+    uncompressWar(sourcePath)
+    decompile(sourcePath, dstPath, jarPath)
+    uncompressJarResult(dstPath)
+    collectAllJarToStoreLib(sourcePath, dstPath)
+    removeSoureWar(sourcePath)
+
+
+def uncompressWar(sourcePath):
+    commandFmt = "find {} -name \"*.war\" -type f | xargs -I xx unzip xx -d xx.folder"
+    command = commandFmt.format(sourcePath)
+    os.system(command)
+
+def decompile(sourcePath, dstPath, jarPath):
+    print(jarPath)
+    commandFmt = "java -cp \"{}\" org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler -dgs=true {} {}"
+    command = commandFmt.format(jarPath, sourcePath, dstPath)
+    print(command)
+    os.system(command)
+
+def uncompressJarResult(dstPath):
+    commandFmt = "find {} -name \"*.jar\" -type f | xargs -I xx unzip xx -d xx.folder "
+    command = commandFmt.format(dstPath)
+    os.system(command)
+    # clean jar
+    commandFmt = "find {} -name \"*.jar\" -type f | xargs -I xx rm -f xx "
+    command = commandFmt.format(dstPath)
+    os.system(command)
+
+def collectAllJarToStoreLib(sourcePath, dstPath):
+    storeLibPath = "{}/storelib".format(dstPath)
+    if os.path.exists(storeLibPath) == False:
+        os.mkdir(storeLibPath)
+    commandFmt = "find {} -name \"*.jar\" -type f | xargs -I xx cp xx {}/"
+    command = commandFmt.format(sourcePath , storeLibPath)
+    os.system(command)lujin
+
+def removeSoureWar(sourcePath):
+    commandFmt = "find {} -name \"*.war\" -type f | xargs -I xx rm -rf xx.folder"
+    command = commandFmt.format(sourcePath)
+    os.system(command)
+
+if __name__ == '__main__':
+    main()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
